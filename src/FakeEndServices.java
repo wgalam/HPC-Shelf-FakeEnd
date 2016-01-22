c928edd6589e48b6a488a2fc9050bd8f6b00f671
wagner <wagner@Bulldozer>
2016-01-22 02:12:06 -0300

    ...

---
+++ src/FakeEndServices.java
@@ -0,0 +1,14 @@
+
+public class FakeEndServices {
+
+	public boolean instantiate(String File){
+		return true;
+	}
+	
+	public boolean addFile(String Unit, byte [] file){
+		return true;
+	}
+	
+	
+
+}
